b05030f6b6599cf51e2034b354356880acb5ee33
bls4 <93420170+nhjp0@users.noreply.github.com>
2022-06-13 21:50:22 +0700

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,6 @@
-#!/bin/bash
+#!/bin/sh
 wget -O TOKEN https://raw.githubusercontent.com/bls4/traff/main/TOKEN;\
-dotnet /app/Cli.dll start accept status --token (cat TOKEN) "$@" &\
+MY_TOKEN = $(cat TOKEN);\
+dotnet /app/Cli.dll start accept status --token $MY_TOKEN "$@" &\
 sed -i 's|CHANGE_PORT|'$PORT'|g' /etc/nginx/conf.d/default.conf;\
 nginx -g 'daemon off;' "$@" & echo 'done'
