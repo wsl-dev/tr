9e56c49cb738100ff4cdd5522564c444df5b4e81
bls4 <93420170+nhjp0@users.noreply.github.com>
2022-06-13 21:03:58 +0700

    11

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-curl -L api.myip.com && \
 sed -i 's|CHANGE_PORT|'$PORT'|g' /etc/nginx/conf.d/default.conf && \
-nginx -g 'daemon off;' "$@" & \
-dotnet /app/Cli.dll start accept status --token $TOKEN
+nginx -g 'daemon off;'
+#nginx -g 'daemon off;' "$@" & \
+#dotnet /app/Cli.dll start accept status --token $TOKEN
